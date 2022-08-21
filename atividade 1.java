bc6470b5d058133ba2c9359ee9103c34da42b680
igortristao <111477272+igortristao@users.noreply.github.com>
2022-08-20 23:29:23 -0300

    Create atividade 1.java

---
+++ atividade 1.java
@@ -0,0 +1,17 @@
+package com.example.atividade1;
+
+import java.util.Scanner;
+
+class HelloApplication {
+    public static void main(String[] args) {
+
+        int A, B, X;
+        Scanner input = new Scanner(System.in);
+
+        A = 10;
+        B = 9;
+        X = A+B;
+
+        System.out.printf("Resultado: " + X);
+        }
+    }
