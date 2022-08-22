cd35978b5e7f1ed38a4a392e675ba83f92d67d55
igortristao <111477272+igortristao@users.noreply.github.com>
2022-08-22 00:06:38 -0300

    Update atividade 2.java

--- atividade 2.java
+++ atividade 2.java
@@ -1,17 +1,17 @@
-1 import java.util.Scanner;
-2
-3	class Main {
-4  public static void main(String[] args) {
-5 
-6    double raio, area;
-7 
-8   Scanner input = new Scanner(System.in);
-9 
-10    raio = input.nextDouble();
-11 
-12    area = 3.14159 * (raio * raio);
-13 
-14    System.out.printf("A=%.4f\n", area);
-15 
-16  }
-17 }
+import java.util.Scanner;
+ 
+class Main {
+  public static void main(String[] args) {
+ 
+    double raio, area;
+ 
+    Scanner input = new Scanner(System.in);
+ 
+    raio = input.nextDouble();
+ 
+    area = 3.14159 * (raio * raio);
+ 
+    System.out.printf("A=%.4f\n", area);
+ 
+  }
+}
