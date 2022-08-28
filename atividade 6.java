7d39779027b39bfb99cecde5579a173fcd892dee
igortristao <111477272+igortristao@users.noreply.github.com>
2022-08-28 14:43:44 -0300

    Create atividade 6.java

---
+++ atividade 6.java
@@ -0,0 +1,24 @@
+
+importar  java . útil . Scanner ;
+
+ questão de classe  pública 6 {
+    public  static  void  main ( String [] args ) {
+        Scanner  enterScanner = new  Scanner ( System . in );
+            duplo  A , B , C ;
+            A = digite Scanner . próximoDouble ();
+            B = digite Scanner . próximoDouble ();
+            C = digite Scanner . próximoDouble ();
+            duplo  MÍDIA ;
+            MÍDIA = ( A * 2 + B * 3 + C * 5 )/ 10 ;
+            Sistema . fora . printf ( "MÍDIA = %.1f" , MÍDIA );
+
+
+
+
+
+
+    }
+
+
+
+}
