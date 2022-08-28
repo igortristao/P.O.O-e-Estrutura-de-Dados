ac4b9a1ebd64a81032acdb3b3d415829aef9c0b5
igortristao <111477272+igortristao@users.noreply.github.com>
2022-08-28 15:07:21 -0300

    atualização

---
+++ atividade 14.java
@@ -0,0 +1,22 @@
+pacote  Atividade ;
+
+importar  java . útil . Scanner ;
+
+ classe  pública questão 14 {
+    public  static  void  main ( String [] args ) {
+
+        Scanner  enterScanner = new  Scanner ( System . in );
+        intx  ; _
+        duplo  y ;
+         consumo duplo ;
+        x = digite Scanner . nextInt ();
+        y = enterScanner . próximoDouble ();
+        consumo = x / y ;
+       Sistema . fora . printf ( String . format ( "%.3f" , consumo ) + " km/l " );
+
+
+
+
+    }
+
+{
