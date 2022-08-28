0f1814395f14eea1d486c158c71c15d4076a2802
igortristao <111477272+igortristao@users.noreply.github.com>
2022-08-28 15:01:16 -0300

    atualização

---
+++ atividade 1.java
@@ -0,0 +1,17 @@
+pacote  com . exemplo . atividade1 ;
+
+importar  java . útil . Scanner ;
+
+class  OláAplicativo {
+    public  static  void  main ( String [] args ) {
+
+        int  A , B , X ;
+         Entrada do scanner = new  Scanner ( System . in );
+
+        A = 10 ;
+        B = 9 ;
+        X = A + B ;
+
+        Sistema . fora . printf ( "Resultado:" + X );
+    }
+} 
---
+++ atividade 8.java
@@ -0,0 +1,23 @@
+pacote com . exemplo . atividade8;
+
+importar  java . útil . Scanner ;
+
+ classe  pública questão8 {
+    public  static  void  main ( String [] args ) {
+        Scanner  enterScanner = new  Scanner ( System . in );
+        int  NÚMERO , horas ;
+        NUMBER = digite Scanner . nextInt ();
+        horas = digite Scanner . nextInt ();
+        valor duplo  Hora ;
+        valorHora = enterScanner . próximoDouble ();
+        duplo  SALÁRIO ;
+        SALÁRIO = ( horas * valorHora );
+
+        Sistema . fora . println ( "NUMERO=" + NUMERO );
+        Sistema . fora . printf ( "SALÁRIO = U$%.2f" , horas * valorHora );
+
+
+
+
+    }
+}
