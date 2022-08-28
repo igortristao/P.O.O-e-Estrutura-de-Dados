7f63649fbbbb422d0cbe81fda1eb0d5d78209d69
igortristao <111477272+igortristao@users.noreply.github.com>
2022-08-28 14:50:59 -0300

    atualização

---
+++ atividade 7.java
@@ -0,0 +1,24 @@
+importar  java . útil . Scanner ;
+
+ classe  pública questão 7 {
+    public  static  void  main ( String [] args ) {
+        Scanner  enterScanner = new  Scanner ( System . in );
+        int  A , B , C , D ;
+
+        A = digite Scanner . nextInt ();
+        B = digite Scanner . nextInt ();
+        C = digite Scanner . nextInt ();
+        D = digite Scanner . nextInt ();
+        int  DIFERENÇA ;
+        DIFERENÇA = ( A * B - C * D );
+
+        Sistema . fora . println ( "DIFERENÇA=" + DIFERENÇA );
+
+
+
+
+
+
+
+    }
+}
